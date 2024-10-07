24573e6b32be55749a2f710b8b8b9474571fd6f3
arafatislam123 <arafatpstu7@gmail.com>
2024-10-08 02:23:13 +0600

    first commit

---
+++ database/migrations/2024_10_07_193902_create_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('email', 50)->unique();
+            $table->string('mobile', 50);
+            $table->string('password'); // you can adjust length if needed, typically bcrypt generates hashes of 60 characters
+            $table->string('OTP', 50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
